81bba9a5e6379916621727e0eefd2f7382791d2c
Tez <tlatocateculti@gmail.com>
2023-08-17 17:16:26 +0200

    Zmiana obsługi menu

--- zdz_17_08_2023/zdz_17_08_2023.cpp
+++ zdz_17_08_2023/zdz_17_08_2023.cpp
@@ -5,7 +5,7 @@
 #include <thread>
 #include <ctime>
 #include <chrono>
-#include <Windows.h>
+#include <windows.h>
 #include <sstream>
 
 using namespace std;
@@ -30,15 +30,15 @@ void printText(string txt) {
     cout << txt;
 }
 
-void setTime(const bool *end) {
+void setTime(const bool *end, const HANDLE* cli, const COORD* pos) {
     tm* czas = new tm;
     for (;;) {
         time_t tme = time(NULL);
         
         localtime_s(czas, &tme);
-        HANDLE cli = GetStdHandle(STD_OUTPUT_HANDLE);
-        COORD pos = GetConsoleCursorPosition(cli);
-        SetConsoleCursorPosition(cli, { 0,0 });
+        //HANDLE cli = GetStdHandle(STD_OUTPUT_HANDLE);
+        //COORD pos = GetConsoleCursorPosition(*cli);
+        SetConsoleCursorPosition(*cli, { 0,0 });
         stringstream ss;
         ss << ((czas->tm_hour < 10) ? "0" : "") << czas->tm_hour << ":" << ((czas->tm_min < 10) ? "0" : "") << czas->tm_min << ":" << ((czas->tm_sec < 10) ? "0" : "") << czas->tm_sec << "\n";
         thread pisz(printText, ss.str());
@@ -46,7 +46,7 @@ void setTime(const bool *end) {
         //cout << ((czas->tm_hour < 10) ? "0" : "") << czas->tm_hour << ":" << ((czas->tm_min < 10) ? "0" : "") << czas->tm_min << ":" << ((czas->tm_sec < 10) ? "0" : "") << czas->tm_sec << "\n";
         
         //cerr << pos.X << " " << pos.Y << "\n";
-        SetConsoleCursorPosition(cli, pos);
+        SetConsoleCursorPosition(*cli, *pos);
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
         if (*end) break;
     }
@@ -57,30 +57,36 @@ void setTime(const bool *end) {
 
 int main()
 {
+    HANDLE cli = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD pos = GetConsoleCursorPosition(cli);
+
+
     bool die = false;
-    thread czas(setTime, &die);
-    
-    
+    thread czas(setTime, &die, &cli, &pos);
+
 
     int menu = 0;
-    string text = "\n\nMenu:\n1. Ustaw godzine\n2.Ustaw minuty\n3.Wyjdz\n\nTwoj wybor: ";
-    thread pisz(printText, text);
-    pisz.join();
-    //cout << "\n\nMenu:\n1. Ustaw godzine\n2.Ustaw minuty\n3.Wyjdz\n\nTwoj wybor: ";
-    
-    cin >> menu;
+    for (;;) {
+        SetConsoleCursorPosition(cli, { 0,0 });
+        std::string text = "\n\nMenu:\n1. Ustaw godzine\n2.Ustaw minuty\n3.Wyjdz\n\nTwoj wybor: ";
+        thread pisz(printText, text);
+        pisz.join();
+        pos = GetConsoleCursorPosition(cli);
+        //cout << "\n\nMenu:\n1. Ustaw godzine\n2.Ustaw minuty\n3.Wyjdz\n\nTwoj wybor: ";
 
-    if (menu == 3) exit(0);
-    if (menu == 1) {
-        thread pisz(printText, "ZMIENIAM GODZINE!\n");
-        pisz.join();
-    }
-    if (menu == 2) {
-        thread pisz(printText, "ZMIENIAM MINUTE!\n");
-        pisz.join();
+        std::cin >> menu;
+
+        if (menu == 3) break;
+        if (menu == 1) {
+            thread pisz(printText, "ZMIENIAM GODZINE!\n");
+            pisz.join();
+        }
+        if (menu == 2) {
+            thread pisz(printText, "ZMIENIAM MINUTE!\n");
+            pisz.join();
+        }
     }
     die = true;
-    //czas.detach();
     czas.join();
     return 0;
 }
